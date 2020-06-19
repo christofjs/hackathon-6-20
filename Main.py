9145770d4143ef5d022ff3dc1737cb889c50999c
Eddie <ewei2406@gmail.com>
2020-06-19 16:50:29 -0700

    Added sentence method to paper Added " " to dictionary Test sentence now in main More things are in the config

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import Dictionary
 
 def main():
     test_paper = Paper("TestNumber1")
-    test_paper.drawSentence("aaaaaaaaaaaaa aaaaaaaa  aaaaaaaaaa aaaaaaaa aaaaa aaaaaaaaa aaaaaaaa aaaa aaaa aaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaaaaaaaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaaa")
+    test_paper.drawSentence("hello this is a test")
     test_paper.show()
 
 
